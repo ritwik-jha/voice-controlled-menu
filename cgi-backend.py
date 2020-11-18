ca88c1980712431d60af69ea271eeea70bc1bc76
Ritwik Jha <59885389+ritwik-jha@users.noreply.github.com>
2020-11-18 11:24:08 +0530

    linux update

--- cgi-backend.py
+++ cgi-backend.py
@@ -13,8 +13,8 @@ while True:
         	06. Set/change password
         	07. Show current RAM usage
         	08. Show the disk usage
-        	09. Check if command is present
-        	10. Removes a command
+        	09. Check if package is present
+        	10. Remove a package
         	11. Lists the network configuration
         	12. Checks the java running services
         	13. List the CPU info
@@ -31,5 +31,61 @@ while True:
 			z = input()
 			str = "mkdir " + z
 			os.system(str)
+		elif y=="2":
+			os.system("ls")
+		elif y=="3":
+			z = input()
+			str = "touch "+ z
+			os.system(str)
+		elif y=="4":
+			z = input()
+			str = "cat " + z
+			os.system(str)
+		elif y=="5":
+			z = input()
+			str = "useradd " + z
+			sp.getoutput(str)
+		elif y=="6":
+			z = input()
+			str = "passwd " + z
+			os.system(str)
+		elif y=="7":
+			os.system("free -m")
+		elif y=="8":
+			os.system("df -hT")
+		elif y=="9":
+			z = input()
+			os.system("rpm -q {}".format(z))
+		elif y=="10":
+			z = input()
+			os.system("rpm -e {}".format(z))
+		elif y=="11":
+			os.system("ifconfig")
+		elif y=="12":
+			os.system("jps")
+		elif y=="13":
+			os.system("lscpu")
+		elif y=="14":
+			os.system("ps -aux")
+		elif y=="15":
+			os.system("uptime")
+		elif y=="16":
+			os.system("echo 3 > /proc/sys/vm/drop_caches")
+		elif y=="17":
+			z = input()
+			os.system("yum whatprovides {}".format(z))
+		elif y=="18":
+			z = input()
+			os.system("ping {}".format(z))
+		elif y=="19":
+			z = input()
+			a = input()
+			os.system("useradd -s {} {}".format(z,a))
+		elif y=="20":
+			continue
+		else:
+            		print("ERROR: Command not found")
+		
+			
 		
 	
